a37a761026d76740bd0760033134ea2e6538591b
ACola03 <166641004+ACola03@users.noreply.github.com>
2024-10-20 00:16:31 -0400

    Add modifyPuzzle.py (Incomplete)     - will finish soon

---
+++ modifyPuzzle.py
@@ -0,0 +1,9 @@
+"""
+This file generates the new puzzle
+State given the legal blank tile 
+Movement. With the new puzzle matrix
+It creates a new node with the 
+Required data.
+"""
+
+def modifyPuzzle()
