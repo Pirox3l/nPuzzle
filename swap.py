e6a1360e4eb44da7db3378a84c2ee4674c3b9d9e
ACola03 <166641004+ACola03@users.noreply.github.com>
2024-10-19 21:49:42 -0400

    Add swap.py to N-puzzle Implementation     - Calculates the third heuristic score     - Often similar to h1 but sometimes more optimistic (lower estimate)

---
+++ swap.py
@@ -0,0 +1,60 @@
+"""
+This file will calculate the 
+Third heuristic score for the
+Number of swaps required to 
+Solve the puzzle. Since this
+Heurisitc is a solution to 
+A relaxed problem, then it
+Is an admissible heuristic
+For the original problem.
+"""
+
+from createPuzzle import createPuzzle
+
+def swap(puzzle):
+    flat = puzzle.flatten()
+    
+    # count swaps
+    swaps = 0
+    
+    for i in range(9): 
+
+        """
+        This loop continues until position i has 
+        the correct tile value. In the process, 
+        the incorrect tile at position i will be
+        moved to its correct location by subtracting
+        its value by 1. Until the correct tile is 
+        swapped to position i, this will continue.
+        It is important to note that the maximum number 
+        of swaps required is 8 but is often less since
+        swapping two tiles in one move can place them
+        both in the correct position.  
+        """
+
+        while flat[i] != 0 and flat[i] != i + 1:
+            # the correct location for the tile at square i
+            correct_pos = flat[i] - 1
+            
+            # swap the value at i to its correct location
+            flat[i], flat[correct_pos] = flat[correct_pos], flat[i]
+            
+            # add swaps
+            swaps = swaps + 1 
+   
+    return swaps
+
+# Driver:
+if __name__ == "__main__":
+    puzzle = createPuzzle(3)
+    swaps = swap(puzzle)
+    print(puzzle)
+    print(swaps)
+
+"""
+A good test case:
+[[0 3 4]
+ [6 7 8]
+ [1 5 2]]
+8
+"""
